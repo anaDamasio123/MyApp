18ea329885b0be405d8f88ece7b18f22b43cc032
anadamasio <2051320@student.uma.pt>
2022-08-21 19:45:05 +0000

    2 way binding

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -6,12 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  name = 'Clean Code';
-  author = 'Robert C. Martin';
+  name: string = 'Clean Code';
+  author: string = 'Robert C. Martin';
+  src: string = 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
+
+  name2: string = 'Pragramatic Programmer';
+  author2: string = ' David Thomas';
+  src2: string = 'https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_UY218_.jpg';
+
+  isDisabled: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleClick() {
+    this.isDisabled = true;
+  }
+
+  myName: string = '';
+  handleInput(event: any) {
+    this.myName = event.target.value;
+  }
 }
