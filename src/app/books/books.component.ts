63944924f98afc5577b1a5fae60f4fe78e448c5d
anadamasio <2051320@student.uma.pt>
2022-08-24 21:08:37 +0100

    ng-if, ng-for and pipes

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,29 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name:string
+  author:string
+  image:string,
+  amount:number
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-  name: string = 'Clean Code';
-  author: string = 'Robert C. Martin';
-  src: string = 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg';
+  books: Book[] = [
+    {
+      name: 'clean code',
+      author: 'robert C. martin',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/41xShlnTZTL._SX376_BO1,204,203,200_.jpg',
+      amount: 700
+    },
+    {
+      name: 'pragramatic programmer',
+      author: ' david thomas',
+      image: 'https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_UY218_.jpg',
+      amount: 800
+    }
+  ]
+  
+  isShowing: boolean = true;
 
-  name2: string = 'Pragramatic Programmer';
-  author2: string = ' David Thomas';
-  src2: string = 'https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_UY218_.jpg';
-
-  isDisabled: boolean = false;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  handleClick() {
-    this.isDisabled = true;
-  }
-
-  myName: string = '';
 }
