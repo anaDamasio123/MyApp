6ed3ef7d7b21c1faad08de5bc0954995415cd155
anadamasio <2051320@student.uma.pt>
2022-08-25 01:03:36 +0100

    styling and some removing

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -25,6 +25,18 @@ export class BooksComponent implements OnInit {
       author: ' david thomas',
       image: 'https://m.media-amazon.com/images/I/51W1sBPO7tL._AC_UY218_.jpg',
       amount: 800
+    },
+    {
+      name: 'art of the computer programming',
+      author: 'donald john fuller',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/41msKv2oN9L._SY429_BO1,204,203,200_.jpg',
+      amount: 500
+    },
+    {
+      name: 'introduction to algorithms',
+      author: 't. connen',
+      image: 'https://mit-press-us.imgix.net/covers/9780262046305.jpg?auto=format&w=260&dpr=1&q=20',
+      amount: 850
     }
   ]
   
